0249921666f31c2447664893ad2291b8cc34b03f
Anonim47 <yosef@yosefadi.tk>
2015-10-05 04:53:18 -0400

    Update scripts
    * Set Max CCACHE size
    * Repo sync before build

--- build_xoplax.sh
+++ build_xoplax.sh
@@ -1,12 +1,20 @@
 #!/usr/bin/env bash
 
-cd ~/xoplax-lp
-make clobber
-cd ~/xoplax-lp
-export PATH=~/bin:$PATH
+echo "Set current directory to source.."
+cd ~/xoplax-$AV
+make clobber | grep Entire
+echo "Set current directory again.."
+cd ~/xoplax-$AV
+export PATH=~/bin:$PATH:/usr/bin:/sbin
+echo "Make sure CCACHE is used.."
 export USE_CCACHE=1
+ccache -M 16G
 source build/envsetup.sh 
+echo "Syncing build tree.."
+repo sync
+echo "Lunching device.."
 lunch cm_$DEVICE-$BUILD_TYPE
+echo "Starting the real build!"
 make bacon
-#brunch cm_rendang-userdebug
-
+echo "Build success!"
+exit
