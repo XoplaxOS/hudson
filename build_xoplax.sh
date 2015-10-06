c7f048c2284a2b8bd615a604ba742d3be5a6ec4d
Anonim47 <yosef@yosefadi.tk>
2015-10-05 22:18:45 -0400

    hudson: enable pipe
    * Faster build proccess - Save time

--- build_xoplax.sh
+++ build_xoplax.sh
@@ -9,12 +9,12 @@ export PATH=~/bin:$PATH:/usr/bin:/sbin
 echo "Make sure CCACHE is used.."
 export USE_CCACHE=1
 ccache -M 16G
+export HOST_USE_PIPE=true
 source build/envsetup.sh 
 echo "Syncing build tree.."
-repo sync -j1
+repo sync -j4
 echo "Lunching device.."
 lunch cm_$DEVICE-$BUILD_TYPE
 echo "Starting the real build!"
-make bacon
-echo "Build success!"
-exit
+make -j5
+
